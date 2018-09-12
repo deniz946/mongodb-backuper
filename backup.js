1881ed2bd90003716c4d1b99e4178295e2a3b00c
deniz946 <deniz946@gmail.com>
2018-09-12 21:36:43 +0200

    Changed way of backing up

--- backup.js
+++ backup.js
@@ -1,14 +1,12 @@
-const backup = require('mongodb-backup');
+const exec = require('child_process').exec;
 const moment = require('moment');
-
+const path = require('path');
 const today = moment().format('DD/MM/YYYY');
 const dbName = process.argv[2];
-
 if (dbName) {
-    backup({
-        uri: `mongodb://localhost:27017/${dbName}`,
-        root: `${__dirname}/${today}`
-    });
+    child = exec(`mongodump --db ${dbName} --out ../${dbName}-${today}`, (error, stdout, stderr) =>{
+        console.log(`${dbName} backed up correctly`);
+    })
 } else {
     console.warn('Error: You should indicate db name');
     return;
