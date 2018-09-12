3bd029e23ac130119c4dca5d5fcb05393b007ef7
deniz946 <deniz946@gmail.com>
2018-09-12 22:11:31 +0200

    Added options list for dbs

--- backup.js
+++ backup.js
@@ -1,14 +1,43 @@
 const exec = require('child_process').exec;
 const moment = require('moment');
 const path = require('path');
-const today = moment().format('DD/MM/YYYY');
+const today = moment().format('DD-MM-YYYY');
 const dbName = process.argv[2];
-if (dbName) {
-    child = exec(`mongodump --db ${dbName} --out ../${dbName}-${today}`, (error, stdout, stderr) =>{
-        console.log(`${dbName} backed up correctly`);
-    })
-} else {
-    console.warn('Error: You should indicate db name');
-    return;
-}
+const inquirer = require('inquirer');
+var MongoClient = require('mongodb').MongoClient;
+
+// Connection url
+var url = 'mongodb://localhost:27017/test';
+MongoClient.connect(url, function(err, db) {
+  // Use the admin database for the operation
+  var adminDb = db.admin();
+  // List all the available databases
+  adminDb.listDatabases(function(err, result) {
+    const avaibleDbs = result.databases.map(db => db.name);
+    const questions = [
+        { type: 'list', name: 'selectedDb', message: 'Choose the db to backup', choices: avaibleDbs },
+    ];
+    
+    inquirer
+        .prompt(questions)
+        .then(function (answers) {
+            console.log(answers);
+        })
+    db.close();
+  });
+});
+
+
+
+
+
+
+// if (dbName) {
+//     child = exec(`mongodump --db ${dbName} --out ../${dbName}-${today}`, (error, stdout, stderr) =>{
+//         console.log(`${dbName} backed up correctly`);
+//     })
+// } else {
+//     console.warn('Error: You should indicate db name');
+//     return;
+// }
 
