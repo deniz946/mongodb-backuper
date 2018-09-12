706d2346f2079d1a52a860e335279f35adc684c9
deniz946 <deniz946@gmail.com>
2018-09-12 22:18:11 +0200

    Changed to mongoose driver

--- backup.js
+++ backup.js
@@ -4,28 +4,39 @@ const path = require('path');
 const today = moment().format('DD-MM-YYYY');
 const dbName = process.argv[2];
 const inquirer = require('inquirer');
-var MongoClient = require('mongodb').MongoClient;
-
+const mongoose = require('mongoose');
 // Connection url
-var url = 'mongodb://localhost:27017/test';
-MongoClient.connect(url, function(err, db) {
-  // Use the admin database for the operation
-  var adminDb = db.admin();
-  // List all the available databases
-  adminDb.listDatabases(function(err, result) {
-    const avaibleDbs = result.databases.map(db => db.name);
-    const questions = [
-        { type: 'list', name: 'selectedDb', message: 'Choose the db to backup', choices: avaibleDbs },
-    ];
+
+mongoose.connect('mongodb://localhost/test');
+
+mongoose.connection.on('open', function (ref) {
+    console.log('Connected to mongo server.');
+    //trying to get collection names
+    mongoose.connection.db.listCollections().toArray((error, collections) => { 
+        console.log(collections) 
+        mongoose.connection.close()
+    })
+
+})
+// var url = 'mongodb://localhost:27017/test';
+// MongoClient.connect(url, function(err, db) {
+//   // Use the admin database for the operation
+//   var adminDb = db.admin();
+//   // List all the available databases
+//   adminDb.listDatabases(function(err, result) {
+//     const avaibleDbs = result.databases.map(db => db.name);
+//     const questions = [
+//         { type: 'list', name: 'selectedDb', message: 'Choose the db to backup', choices: avaibleDbs },
+//     ];
     
-    inquirer
-        .prompt(questions)
-        .then(function (answers) {
-            console.log(answers);
-        })
-    db.close();
-  });
-});
+//     inquirer
+//         .prompt(questions)
+//         .then(function (answers) {
+//             console.log(answers);
+//         })
+//     db.close();
+//   });
+// });
 
 
 
