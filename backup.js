89f1733ab1d0ae70fd893df6029a478b3146f690
deniz946 <deniz946@gmail.com>
2018-09-16 19:12:30 +0200

    is this the def version?

--- backup.js
+++ backup.js
@@ -2,43 +2,46 @@ const exec = require('child_process').exec;
 const moment = require('moment');
 const path = require('path');
 const today = moment().format('DD-MM-YYYY');
-const dbName = process.argv[2];
+const dbName2 = process.argv[2];
 const inquirer = require('inquirer');
+
+
 const MongoClient = require('mongodb').MongoClient;
-// Connection URL
-const url = 'mongodb://localhost:27017/test';
+const test = require('assert');
+const url = 'mongodb://localhost:27017';
+const dbName = 'test';
+MongoClient.connect(url, function (err, client) {
+    // Use the admin database for the operation
+    const adminDb = client.db(dbName).admin();
+    // List all the available databases
+    adminDb.listDatabases(function (err, dbs) {
+        const avaibleDbs = dbs.databases.map(db => db.name);
+        const questions = [
+            { type: 'list', name: 'selectedDb', message: 'Choose the db to backup', choices: avaibleDbs },
+        ];
 
-// Database Name
-
-// // Use connect method to connect to the server
-// MongoClient.connect(url, function (err, client) {
-//     console.log("Connected successfully to server");
-//     const db = client.db('ngClients');
-//     console.log(db);
-//     client.close();
-// });
-
-
-
-mongoose.connection.on('error', function(error){
-  throw new Error(error);
+        inquirer
+            .prompt(questions)
+            .then(function (answers) {
+                console.log(answers);
+                if (answers.selectedDb) {
+                    const db = answers.selectedDb;
+                    child = exec(`mongodump --db ${db} --out ../${db}-${today}`, (error, stdout, stderr) => {
+                        console.log(`${db} backed up correctly`);
+                    })
+                } else {
+                    console.warn('Error: You should indicate db name');
+                    return;
+                }
+                client.close();
+            })
+    });
 });
 // MongoClient.connect(url, function(err, db) {
 //   // Use the admin database for the operation
 //   var adminDb = db.admin();
 //   // List all the available databases
-//   adminDb.listDatabases(function(err, result) {
-//     const avaibleDbs = result.databases.map(db => db.name);
-//     const questions = [
-//         { type: 'list', name: 'selectedDb', message: 'Choose the db to backup', choices: avaibleDbs },
-//     ];
 
-//     inquirer
-//         .prompt(questions)
-//         .then(function (answers) {
-//             console.log(answers);
-//         })
-//     db.close();
 //   });
 // });
 
