b57f00db1c2b5e227dd9380378bf5c437e79159b
deniz946 <deniz946@gmail.com>
2018-09-16 19:57:36 +0200

    Improved structure

--- backup.js
+++ backup.js
@@ -5,28 +5,30 @@ const fs = require('fs');
 const today = moment().format('DD-MM-YYYY');
 const inquirer = require('inquirer');
 const shelljs = require('shelljs');
-
 const MongoClient = require('mongodb').MongoClient;
-const test = require('assert');
 const url = 'mongodb://localhost:27017';
 const dbName = 'test';
+
+
+checkNodeModules();
 console.log('Connecting to the database...');
-MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
+MongoClient.connect(url, { useNewUrlParser: true }, onConnectToDB);
+
+function onConnectToDB(err, client) {
     // Use the admin database for the operation
     const adminDb = client.db(dbName).admin();
     // List all the available databases
-    adminDb.listDatabases(function (err, dbs) {
+    adminDb.listDatabases((err, dbs) => {
         const avaibleDbs = dbs.databases.map(db => db.name);
-        const questions = [
+        const question = [
             { type: 'list', name: 'selectedDb', message: 'Choose the db to backup', choices: avaibleDbs },
         ];
 
         inquirer
-            .prompt(questions)
-            .then(function (answers) {
-                console.log(answers)
-                if (answers.selectedDb) {
-                    makeBackup(answers.selectedDb);
+            .prompt(question)
+            .then(answer => {
+                if (answer.selectedDb) {
+                    doBackup(answer.selectedDb);
                 } else {
                     console.warn('Error: You should indicate db name');
                     return;
@@ -34,25 +36,33 @@ MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
                 client.close();
             })
     });
-});
+}
 
 
-function makeBackup(selectedDb) {
+function doBackup(selectedDb) {
     const dbLocalFilesPath = `../${selectedDb}`;
-
     if (fs.existsSync(dbLocalFilesPath)) {
-        console.log('There are no folder for this db, creating...');
-        mongoDumpFunction(selectedDb);
-    } else {
         console.log('We\'ve found backups folder for this db... backing up there');
+    } else {
+        console.log('There are no folder for this db, creating...');
         shelljs.mkdir(dbLocalFilesPath);
-        mongoDumpFunction(selectedDb);
     }
+
+    mongoDumpFunction(selectedDb);
 }
 
 function mongoDumpFunction(selectedDb) {
     const pathToBackup = `../${selectedDb}/${selectedDb}-${today}`;
     child = exec(`mongodump --db ${selectedDb} --out ${pathToBackup}`, (error, stdout, stderr) => {
+        if (error) {
+            throw (error);
+        }
         console.log(`${selectedDb} backed up correctly in ${pathToBackup}`);
     })
+}
+
+function checkNodeModules() {
+    if (!sh.test('-e', './node_modules')) {
+        throw ('Run npm install to install the libs I need to run your backup');
+    }
 }
